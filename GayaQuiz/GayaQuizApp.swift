72e316c1eb4271a515f6450fff2b457548fc8abb
Allan <allan.viana2003@gmail.com>
2024-11-18 01:45:46 -0300

    Initial Commit

---
+++ GayaQuiz/GayaQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  GayaQuizApp.swift
+//  GayaQuiz
+//
+//  Created by Allan Viana on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GayaQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GayaQuizTests/GayaQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  GayaQuizTests.swift
+//  GayaQuizTests
+//
+//  Created by Allan Viana on 18/11/24.
+//
+
+import Testing
+@testable import GayaQuiz
+
+struct GayaQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
